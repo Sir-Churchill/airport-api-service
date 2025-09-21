a59d79ecf42619b2d036ecd56ef52cc4fa0198f7
Sir-Churchill <trickster@MacBook-Air-Trickster.local>
2025-09-22 00:27:45 +0200

    feat(airport): add viewsets for core models

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import Airport, Route, Airplane, Flight, Order, Ticket, AirplaneType
+from airport.serializers import AirportSerializer, RouteSerializer, AirplaneSerializer, FlightSerializer, \
+    OrderSerializer, AirplaneTypeSerializer, RouteListSerializer, AirplaneListSerializer, FlightListSerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):  #Dont need list
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+        return RouteSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AirplaneListSerializer
+        return AirplaneSerializer
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):  #Dont need list
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return FlightListSerializer
+        return FlightSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
