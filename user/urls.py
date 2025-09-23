e03413d73649e638b62cf42f3212c4e3bec68db2
Sir-Churchill <trickster@MacBook-Air-Trickster.local>
2025-09-24 00:04:07 +0200

    feat(user): add me/ endpoint for current user

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 
 app_name = "user"
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", ManageUserView.as_view(), name="me"),
 ]
