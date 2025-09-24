d7632dedb22dbef271860dbd91d9b191fdd4154b
Sir-Churchill <trickster@MacBook-Air-Trickster.local>
2025-09-24 12:15:53 +0200

    config(django): switch database to PostgreSQL via env vars and add description to spectacular_settings

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -82,8 +83,17 @@ WSGI_APPLICATION = 'airport_api_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': os.environ["POSTGRES_DB"],
+
+        'USER': os.environ["POSTGRES_USER"],
+
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
+
+        'HOST': os.environ["POSTGRES_HOST"],
+
+        'PORT': os.environ["POSTGRES_PORT"],
     }
 }
 
@@ -156,7 +166,7 @@ REST_FRAMEWORK = {
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Airport API Service',
-    'DESCRIPTION': 'Your project description',
+    'DESCRIPTION': 'Manage airports, flights, airplanes, routes, crews, and ticket orders via API.',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
